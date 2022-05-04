9b1c1a98e75ee142cf2f20a22c9f5b8973af6acd
chek <heychek@foxmail.com>
2022-05-05 01:09:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 一、简介
+本项目是 SpringSecurity 学习的示例项目
---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 创建于 2022/5/5 01:04
+ *
+ * @author chek
+ */
+@Configuration
+public class SecurityConfig {
+
+}
